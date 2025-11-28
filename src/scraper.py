e2d792fc0ec76b6ff60fd9990c7aeebf347efec5
DamyanBG <d.dimitrov.1988@abv.bg>
2025-11-28 12:13:57 +0200

    fix changes in lidl scraping

--- src/scraper.py
+++ src/scraper.py
@@ -1,6 +1,6 @@
 import re
 
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, ElementHandle
 
 from logger import logger
 
@@ -9,21 +9,57 @@ def scrape_url_links(week_dates: str) -> list[str]:
     img_links: list[str] = []
 
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
+        browser = p.chromium.launch(headless=False)
         page = browser.new_page()
 
         logger.info("Browser initiated.")
 
-        # Open the website
-        page.goto(f"https://www.lidl.bg/l/bg/broshura/{week_dates}/view/flyer/page/1")
+        page.goto("https://www.lidl.bg/")
 
         logger.info("Accept cookies")
         try:
             page.click("#onetrust-accept-btn-handler", timeout=10000)
         except Exception as e:
             logger.error(str(e))
-            logger.error("Error on click on brochure")
-            raise Exception("Error on click on brochure")
+            logger.error("Error on click on accept cookie")
+            raise Exception("Error on click on accepct cookie")
+
+        try:
+            page.click("a.n-navigation__menu-nav--link.n-header__icon-link", timeout=5000)
+        except Exception as e:
+            logger.error(str(e))
+            logger.error("Error on click on brochures link")
+            raise Exception("Error on click on brochures link")
+
+        page.wait_for_selector("a.flyer")
+        a_els = page.query_selector_all("a.flyer")
+
+        logger.info("Selected flyer elements")
+
+        print(len(a_els))
+
+        def find_correct_link(el: ElementHandle) -> bool:
+            href = el.get_attribute("href")
+            if not href:
+                return False
+            print(href)
+            return week_dates in href
+
+        a_el = next(filter(find_correct_link, a_els), None)
+
+        if not a_el:
+            raise Exception("No element found for the flyer for the next week!")
+
+        logger.info("Found element for the brochure")
+
+        brochure_link = a_el.get_attribute("href")
+        if not brochure_link:
+            raise Exception("No element found for the flyer for the next week!")
+
+        # Open the website
+        page.goto(brochure_link)
+
+        logger.info("page opened")
         
         while True:
             # Extract image links on current page
